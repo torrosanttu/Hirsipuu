40ba56f5985d66347213b240d4112616b60097ea
torrosanttu <santtu.torro@gmail.com>
2024-02-21 02:56:01 +0200

    Projekti hirsipuu valmis.

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,7 @@
-const input = document.querySelector('input')
-const ouput = document.querySelector('output')
-const span = document.querySelector('span')
+const input = document.querySelector('input');
+const output = document.querySelector('output');
+const span = document.querySelector('span');
+var arvaukset = 0
 
 const words = [
     "programming",
@@ -13,53 +14,56 @@ const words = [
     "library",
     "asynchronous",
     "hypertext"
-]
+];
 
-let randomizedWord = ''
-let maskedWord = ''
+let randomizedWord = '';
+let maskedWord = '';
 
 const newGame = () => {
-    const random = Math.floor(Math.random() * 10) + 1
-    randomizedWord = words[random]
-    maskedWord = "*".repeat(randomizedWord.length)
-    console.log(randomizedWord)
-    ouput.innerHTML = maskedWord
-}
+    const random = Math.floor(Math.random() * words.length);
+    randomizedWord = words[random];
+    maskedWord = "*".repeat(randomizedWord.length);
+    console.log(randomizedWord);
+    output.innerHTML = maskedWord;
+    arvaukset = 0
+};
 
 const win = () => {
-    alert(`You have guessed right, the word is ${randomizedword}.`)
-    newGame()
-}
+    alert(`You have guessed right, the word is ${randomizedWord}. You guessed ${arvaukset} times!`);
+    newGame();
+};
 
 const replaceFoundChars = (guess) => {
-    for (let i = 0;i<randomizedWord.length;i++) {
-        const char = randomizedWord.substring(i,i+1)
+    guess = guess.toLowerCase();
+    for (let i = 0;i  < randomizedWord.length; i++) {
+        const char = randomizedWord.substring(i, i + 1).toLowerCase();
         if (char === guess) {
-            let newString = maskedWord.split('')
-            newString.splice(i,1,guess)
-            newString = newString.join('')
-            maskedWord = newString
+            let newString = maskedWord.split('');
+            newString.splice(i,1,guess);
+            maskedWord = newString.join('');
         }
     }
-    ouput.innerHTML = maskedWord
+    output.innerHTML = maskedWord;
 }
-newGame()
+newGame();
 
 input.addEventListener('keypress',(e) => {
-    if (e.key ==='Enter') {
-        e.preventDefault()
+    if (e.key === 'Enter') {
+        e.preventDefault();
 
-        const guess = input.value
+        const guess = input.value;
         if (guess.toLowerCase() === randomizedWord.toLowerCase()) {
-            win()
+            win();
         } else if (guess.length === 1) {
-            replaceFoundChars(guess)
+            replaceFoundChars(guess);
             if (maskedWord.toLocaleLowerCase() === randomizedWord.toLocaleLowerCase()) {
-                win()
+                win();
             }
         } else {
-            alert("You guessed wrong!")
+            alert("You guessed wrong!");
         }
-        input.value=''
+        input.value='';
+
+        arvaukset++;
     }
-})
+});
